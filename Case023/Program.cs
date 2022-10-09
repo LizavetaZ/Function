f8628373c6a1db54679bff33edaca7a5a0187886
Лиза за <lizuska@yandex.ru>
2022-10-09 15:25:25 +0300

    Cube table

---
+++ Case023/Program.cs
@@ -0,0 +1,24 @@
+﻿int InputValue(string message)
+{
+    Console.WriteLine(message);
+    string val = Console.ReadLine();
+    int a = int.Parse(val);
+    return a;
+}
+
+int a = InputValue("Введите число");
+if (a > 0)
+{
+    for (int i = 1; i<=a; i++)
+    {
+        Console.Write(Math.Pow(i,3) +",");
+    }
+}
+else if (a<0)
+{
+    for (int i = a; i<0; i++)
+    {
+        Console.Write(Math.Pow(i,3) +",");
+    }
+}
+else Console.WriteLine("ошибка");
